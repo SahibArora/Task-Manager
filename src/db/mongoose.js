992efd5b47e6a6ae0ee4e27bdd42dc41c647d763
Sahib Arora <sahibarora1997@gmail.com>
2021-01-09 08:28:08 -0500

    Set up cloud mongo-db Server and database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(('mongodb://127.0.0.1:27017/task-manager-api'),{
+mongoose.connect(('mongodb+srv://admin:admin1997@cluster0.vjfiu.mongodb.net/task-manager-api?retryWrites=true&w=majority'),{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
